9a5b4d7e67fb841ffd29a632aa01d8c30ee2e11b
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2020-11-18 22:12:21 +0100

    Added new font files.

--- DaVinci/davinci.py
+++ DaVinci/davinci.py
@@ -6,6 +6,8 @@ import os
 
 POSITION_TEXT = (50, 20)
 OUTPUT_FILE = 'output.txt'
+PARCHMENT_IMG = 'dat/parchment.jpg'
+STYLES_FOLDER = 'dat/styles/'
 
 def uniform_dir_path(directory):
     '''
@@ -27,27 +29,35 @@ def uniform_dir_path(directory):
         return directory+'/'
 
 
-def generate_script(text, max_length, input_path, filename, output_path):
-    global POSITION_TEXT
+def generate_script(text, max_length, input_path, filename, output_path, style):
+    '''
+    Generate Da Vinci's mirror script
+    '''
 
-    img = Image.open('dat/parchment.jpg')
+    global POSITION_TEXT, PARCHMENT_IMG, STYLES_FOLDER, STYLES
+
+    print('Generating script...')
+    img = Image.open(PARCHMENT_IMG)
     img2 = img.copy()
 
     string_text = '\n'
     for x in text:
         string_text = string_text + x + '\n'
 
-    fnt = ImageFont.truetype('dat/styles/Allura-Regular.otf', 40)
+    fnt = ImageFont.truetype(STYLES_FOLDER+STYLES[style], 40)
     d = ImageDraw.Draw(img2)
     d.text(POSITION_TEXT, string_text, font=fnt, fill=(0, 0, 0))
     
     img2 = img2.transpose(Image.FLIP_LEFT_RIGHT)
-
-    if filename:
-        img2.save(output_path+filename.replace('.txt', '.png'))
+    img2.save(output_path+filename.replace('.txt', '.png'))
+    print('Completed creation of script', 'red', end='\n\n')
 
 
 def list_lines(f):
+    '''
+    Return list composed by lines in stream specified by f
+    '''
+
     #List of lines
     text = []
     #Max size of a line
@@ -65,6 +75,10 @@ def list_lines(f):
 
 
 def get_input(input_file):
+    '''
+    Get input in interactive mode or from a file
+    '''    
+
     #List of lines
     text = []
     #Max size of a line
@@ -73,14 +87,27 @@ def get_input(input_file):
     #If specified input file
     if input_file:
 
-        #Save each line, removing final '\n' character
+        print('Opening', end=' ')
+        cprint('{input_file}', end=' ')
+        print('text file to read its content', end='\n\n')
+
+        #Save each line of input_file, removing final '\n' character
         with open(input_file) as f: 
             text, max_length = list_lines(f)
 
     else:
 
         try:
+            cprint('Write the input text', 'red', end=' ')
+            print('(', end='')
+            cprint('CTRL+D', 'green', end=' ')
+            print('to stop insertion and generate script)')
+            cprint('_________________________________________________________________', 'red')
+
+            #Save each line of std_in, removing final '\n' character
             text, max_length = list_lines(stdin)
+            cprint('_________________________________________________________________', 'red', end='\n\n')
+
         except KeyboardInterrupt:
             cprint('\n[Keyboard Interrupt]', 'blue', end=' ')
             print('The user blocks the input using', end=' ')
@@ -91,10 +118,48 @@ def get_input(input_file):
     return text, max_length
 
 
-'''
-Parser of command line arguments
-'''
+def select_style():
+    '''
+    Select style from style files in styles folder
+    '''
+    
+    global STYLES_FOLDER, STYLES
+    check = True
+    STYLES  = os.listdir(STYLES_FOLDER)
+    STYLES.sort()
+    files_no_extension = [x[:-4] for x in STYLES]
+
+    while check:
+        try:
+            cprint('\nSelect which style you want by inserting the corresponding number', 'red')
+            cprint('_________________________________________________________________', 'red')
+            count=0
+            for f in files_no_extension:
+                cprint(f'{count})', 'yellow', end=' ')         
+                print(f)
+                count = count + 1
+            
+            cprint('_________________________________________________________________', 'red')
+            num = int(input())
+            
+            if num>=0 and num < len(files_no_extension):
+                check = False
+
+        except ValueError:
+            cprint('[VALUE ERROR]', 'blue', end=' ')
+            print('you must enter an integer')
+        except KeyboardInterrupt:
+            exit(0)
+
+    print('')
+    return num
+
+
 def args_parser():
+    '''
+    Parser of command line arguments
+    '''
+
     global OUTPUT_FILE
 
     #Parser of command line arguments
@@ -149,9 +214,11 @@ def args_parser():
 def main():
     #Parser of command line arguments
     input_path, output_path, filename = args_parser()    
+    #Select style
+    style = select_style()
     #Take input
     text, max_length = get_input(input_path)
-    generate_script(text, max_length, input_path, filename, output_path)
+    generate_script(text, max_length, input_path, filename, output_path, style)
 
     print(text)
 
