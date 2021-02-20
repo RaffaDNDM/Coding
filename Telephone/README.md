77316bf06106cf370641d7adfb03a2df803bcad0
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2021-02-20 22:55:10 +0100

    Updated program.

--- Telephone/README.md
+++ Telephone/README.md
@@ -1,2 +1,6 @@
-# Jarvis
-The program checks the geography zone of italy related to home telephone numbers or original operators of mobile telephone numbers.
+# Telephone
+The program printss the geography zone of italy related to home telephone numbers, original operators of mobile telephone numbers or service related to a special number.
+It requires several dependencies, that you can install using this bash command:
+```bash
+pip3 install termcolor csv
+```
--- Telephone/telephone.py
+++ Telephone/telephone.py
@@ -4,11 +4,18 @@ import csv
 LINE = '___________________________________________________'
 
 class TelephoneParser:
-    __ZONES_FILENAME = 'prefix_zones.csv'
+    ZONES_PATH = 'prefix_zones.csv'
+    OPERATORS_FILENAME = 'prefix_operators.csv'
+    SPECIAL_FILENAME = 'special_numbers.csv'
     __ZONES = {}
+    __OPERATORS = {}
+    __SPECIAL_NUMS = {}
 
-    def __init__(self):
-        with open(self.__ZONES_FILENAME, 'r') as f:
+    def __init__(self, zones_path=ZONES_PATH, 
+                       operators_path=OPERATORS_FILENAME, 
+                       special_path=SPECIAL_FILENAME):
+
+        with open(zones_path, 'r') as f:
             csv_reader = csv.reader(f, delimiter=',')
 
             for row in csv_reader:
@@ -17,21 +24,33 @@ class TelephoneParser:
                 else:
                     self.__ZONES[row[0]].append((row[1],row[2]))
 
-    def info(self, phone_num):
-        #Invalid number <3 digits or >12 digits
-        if len(phone_num) < 3 or len(phone_num)>15:
-            print('Prefix not found', end='\n\n')
-            return
+        with open(operators_path, 'r') as f:
+            csv_reader = csv.reader(f, delimiter=',')
+            self.__OPERATORS={row[0]:row[1] for row in csv_reader}
 
-        if phone_num[:3] == '+39':
+        with open(special_path, 'r') as f:
+            csv_reader = csv.reader(f, delimiter=',')
+            self.__SPECIAL_NUMS={row[0]:row[1] for row in csv_reader}
+
+    def info(self, phone_num):
+
+        if len(phone_num) > 2 and phone_num[:3] == '+39':
             phone_num = phone_num[3:]
 
-        if len(phone_num) < 6:
-            print('Prefix not found', end='\n\n')
+        if phone_num.startswith('3'):
+            self.operator_from_num(phone_num)
+        elif phone_num.startswith('0'):
+            self.zone_from_num(phone_num)
+        else:
+            self.special_num(phone_num)
+
+    def zone_from_num(self, phone_num):
+        if len(phone_num) < 6 or len(phone_num) > 11:
+            print('Invalid number', end='\n\n')
             return
 
         prefix = ''
-
+        
         for i in range(2, 6):
             if phone_num[:i] in self.__ZONES:
                 prefix = phone_num[:i]
@@ -41,6 +60,34 @@ class TelephoneParser:
             print('Prefix not found', end='\n\n')
         else:
             self.print_zones(prefix)
+
+    def operator_from_num(self, phone_num):
+        if len(phone_num) < 9 or len(phone_num)>10:
+            print('Invalid number', end='\n\n')
+            return
+
+        prefix = ''
+        
+        for i in range(0, 2):
+            if phone_num[:(4-i)] in self.__OPERATORS:
+                prefix = phone_num[:(4-i)]
+                break
+
+        if prefix == '':
+            print('Prefix not found', end='\n\n')
+        else:
+            cprint(self.__OPERATORS[prefix])
+
+    def special_num(self, phone_num):
+        if len(phone_num) < 3:
+            print('Invalid number', end='\n\n')
+            return
+
+        if phone_num in self.__SPECIAL_NUMS:
+            cprint(self.__SPECIAL_NUMS[phone_num], 'yellow', end='\n\n')
+        else:
+            print('Prefix not found', end='\n\n')
+
 
     def print_zones(self, prefix : str):
         for (region, town) in self.__ZONES[prefix]:
