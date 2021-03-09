1b9c9e8e9936e47e03b6e4b77daef2a3b7dba7cc
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2021-03-09 10:55:45 +0100

    Video2GIF converter.

---
+++ Video2GIF/README.md
@@ -0,0 +1,6 @@
+# Video to GIF converter
+The program converts a video file to a GIF file.
+It requires several dependencies, that you can install using this bash command:
+```bash
+pip3 install moviepy argparse termcolor
+```
---
+++ Video2GIF/video2gif.py
@@ -0,0 +1,38 @@
+from moviepy.editor import VideoFileClip
+import argparse
+from termcolor import cprint
+
+def video2gif(video_path, gif_path='output.gif'):
+    video = VideoFileClip(video_path)
+    video.write_gif(gif_path)
+
+'''
+Parser of command line arguments
+'''
+def args_parser():
+    #Parser of command line arguments
+    parser = argparse.ArgumentParser()
+    
+    #Initialization of needed arguments
+    parser.add_argument("-input", "-i", '-video', '-v', dest="input", help="Path of the existing video file to be converted.")
+    parser.add_argument("-gif", dest="gif", help="Path of the output GIF file (output.gif by default).")
+
+    #Parse command line arguments
+    args = parser.parse_args()
+
+    if not args.input:
+        cprint('You need to specify the input video path', 'red')
+        exit()
+
+    return args.input, args.gif
+
+def main():
+    video_path, gif_path = args_parser()
+    
+    if not gif_path:
+        video2gif(video_path)
+    else:
+        video2gif(video_path, gif_path)
+
+if __name__=='__main__':
+    main()
